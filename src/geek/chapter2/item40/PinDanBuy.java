00a25b959e7a9d6bffb999c498718900e797f311
郭新晔 <guoxinye@maoyan.com>
2020-10-30 00:16:00 +0800

    剑指offer:custom

--- src/geek/chapter2/item40/PinDanBuy.java
+++ src/geek/chapter2/item40/PinDanBuy.java
@@ -5,6 +5,9 @@
  */
 package geek.chapter2.item40;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 拼单购买
  * 比如“满 200 元减 50 元”。假设你女朋友的购物车中有 n 个（n>100）想买的商品，
@@ -40,8 +43,60 @@ public class PinDanBuy {
         return 0;
     }
 
+    private void pinDanBuy2(int discount, int[] prices) {
+        int number = prices.length;
+        boolean[][] dp = new boolean[number][discount * 3 + 1];
+
+        // 初始状态
+        // 不买第一件物品
+        dp[0][0] = true;
+        if (prices[0] <= 3 * discount) {
+            // 买第一件物品
+            dp[0][prices[0]] = true;
+        }
+
+        for (int i = 1; i < number; i++) {
+            // 不买第i件物品
+            for (int j = 0; j <= discount * 3; j++) {
+                if (dp[i - 1][j]) {
+                    dp[i][j] = true;
+                }
+            }
+
+            // 买该物品
+            for (int j = 0; j <= discount * 3 - prices[i]; j++) {
+                if (dp[i - 1][j]) {
+                    dp[i][j + prices[i]] = true;
+                }
+            }
+        }
+
+        int result = -1;
+        for (int i = discount; i <= discount * 3; i++) {
+            if (dp[number - 1][i]) {
+                result = i;
+                break;
+            }
+        }
+        System.out.println("result = " + result);
+        List<Integer> buys = new ArrayList<>();
+
+        int mon = result;
+        for (int i = number - 1; i > 0; i--) {
+            if (dp[i - 1][mon - prices[i]]) {
+                buys.add(prices[i]);
+                mon = mon - prices[i];
+            }
+        }
+        if (mon > 0) {
+            buys.add(prices[0]);
+        }
+        System.out.println("buys = " + buys);
+    }
+
     public static void main(String... args) {
         PinDanBuy pinDanBuy = new PinDanBuy();
         System.out.println(pinDanBuy.pinDanBuy(210, new int[]{23, 34, 45, 34, 18, 22, 99, 11, 35}));
+        pinDanBuy.pinDanBuy2(210, new int[]{23, 34, 45, 34, 18, 22, 99, 11, 35});
     }
 }
---
+++ src/geek/chapter2/item40/YanghuiTriangle.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020 maoyan.com
+ * All rights reserved.
+ *
+ */
+package geek.chapter2.item40;
+
+/**
+ * “杨辉三角”不知道你听说过吗？我们现在对它进行一些改造。
+ * 每个位置的数字可以随意填写，经过某个数字只能到达下面一层相邻的两个数字。
+ * 假设你站在第一层，往下移动，我们把移动到最底层所经过的所有数字之和，定义为路径的长度。
+ * 请你编程求出从最高层移动到最底层的最短路径长度。
+ * https://time.geekbang.org/column/article/74788
+ *
+ * @author xingye
+ * @created 2020/10/30
+ */
+public class YanghuiTriangle {
+
+    private void findShortestPath(int[][] origins) {
+        // 从(0,0)到(x,len-1-x),只能向右或向下
+
+    }
+}
